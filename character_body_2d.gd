25547ae5104a51321142809afb5825726d134b32
avic-acid <aviothicacid@gmail.com>
2026-09-21 16:47:14 -0400

    added hamster

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -16,10 +16,13 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("ui_left", "ui_right")		
 	if direction:
 		velocity.x = direction * SPEED
+		$AnimatedSprite2D.flip_h = direction < 0
+		$AnimatedSprite2D.play("run")
 	else:
+		$AnimatedSprite2D.play("idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
