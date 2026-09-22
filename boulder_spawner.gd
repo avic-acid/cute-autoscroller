df3cb3f7b51e802eea0af207c41658e805d88d38
avic-acid <aviothicacid@gmail.com>
2026-09-22 21:55:20 +0000

    moved boulder around and changed files

---
+++ boulder_spawner.gd
@@ -0,0 +1,13 @@
+extends Node2D
+#
+#var boulder : Resource
+#
+# Called when the node enters the scene tree for the first time.
+#func _ready() -> void:
+	#boulder = load("res://boulder.tscn")
+#
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#if Input.is_action_just_pressed("spawn-boulder-debug"):
+		#var current_boulder = boulder.()
+		#add_child(current_boulder)
